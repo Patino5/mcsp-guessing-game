4dc4e38821a846c3adc712709857058796eaf687
Patino5 <ryan.patino5@gmail.com>
2023-10-07 04:18:30 +1000

    added feature 5 name of guesser

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ let randomNumber = Math.round(Math.random() * 100) + 1;
 // generate variables guess and guesses 
 let guess = undefined;
 let guesses = [];
+let user = prompt("what is your name?"); 
+
 
 
 while (guess !== randomNumber){
-    guess = parseInt(prompt("Guess a number between 1 and 100."))
+    guess = parseInt(prompt(`${user}, guess a number between 1 and 100.`))
     guesses.push(guess)
     if (randomNumber === guess){
-        alert(`You are Right! It took you ${guesses.length} guesses. These are the numbers you guessed ${guesses}.`)
+        alert(`Correct, ${user}! It took you ${guesses.length} guesses. These are the numbers you guessed ${guesses}.`)
     } else if (guess > randomNumber)  {
-        alert("Too high, guess lower")
+        alert(`Too high ${user}, guess lower.`)
     } else if (guess < randomNumber){
-        alert("Too low, guess higher")
+        alert(`Too low ${user}, guess higher.`)
     }
 }
